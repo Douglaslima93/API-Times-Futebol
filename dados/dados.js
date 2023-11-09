e70e2ef58fe63974d0e8f794b49818313945c8e9
Douglas Lima <douglas07a@gmail.com>
2023-11-09 11:33:20 -0300

    Inicio do projeto

---
+++ dados/dados.js
@@ -0,0 +1,19 @@
+const times = [
+    {
+        id: 1, nome: "Botafogo", sigla: "BO"
+    },
+    {
+        id: 2, nome: "Corinthians", sigla: "CO"
+    },
+    {
+        id: 3, nome: "São Paulo", sigla: "SP"
+    },
+    {
+        id: 4, nome: "Palmeiras", sigla: "PA"
+    },
+    {
+        id: 5, nome: "Santos", sigla: "SA"
+    }
+];
+
+export default times;
